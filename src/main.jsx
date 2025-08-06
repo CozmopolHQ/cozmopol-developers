ef35be29bdf4798fc8311632f4a5ed222541ea16
Eren Tın <94992378+erentin@users.noreply.github.com>
2025-08-06 19:18:30 +0300

    Dark mode özelliğini tamamen kaldır

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { ThemeProvider } from './contexts/ThemeContext'
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
